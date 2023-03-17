84ec83a3c42d564ba70eab14ae672d14505d5c26
trotyl <mail@trotyl.xyz>
2023-03-17 12:25:28 -0400

    pyqt and map communication

--- map.py
+++ map.py
@@ -1,10 +1,13 @@
 import sys
 import io
 import os
+import json
+import random
 from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout
 from PyQt5.QtWebEngineWidgets import QWebEngineView  # pip install PyQtWebEngine
 from PyQt5 import QtCore
-from PyQt5.QtCore import QUrl
+from PyQt5.QtCore import *
+
 
 """
 Mapbox in PyQt5
@@ -21,10 +24,26 @@ class MyApp(QWidget):
         layout = QVBoxLayout()
         self.setLayout(layout)
 
-
         webView = QWebEngineView()
         webView.load(QUrl('http://localhost:8888/map.html'))
         layout.addWidget(webView)
+        with open("map.json", "r") as jsonFile:
+            data = json.load(jsonFile)
+
+        data["coordinate"] = [-71.6516848, 48.5107057]
+        
+        with open("map.json", "w") as jsonFile:
+            json.dump(data, jsonFile)
+
+
+def reloadMap():
+    with open("map.json", "r") as jsonFile:
+        data = json.load(jsonFile)
+
+    data["coordinate"] = [data["coordinate"][0]+0.00001,data["coordinate"][1]+0.00001]
+    
+    with open("map.json", "w") as jsonFile:
+        json.dump(data, jsonFile)
 
 
 if __name__ == '__main__':
@@ -34,8 +53,12 @@ if __name__ == '__main__':
             font-size: 35px;
         }
     ''')
-
+    timer = QTimer()
+    timer.timeout.connect(reloadMap)  # execute `display_time`
+    timer.setInterval(50)  # 1000ms = 1s
+    timer.start()
     myApp = MyApp()
+
     myApp.show()
 
     try:
